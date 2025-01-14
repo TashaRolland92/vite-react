96049d8890df4c67d8d59b713733f04bb8db74fa
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-01-14 20:09:41 +0000

    hero image and scss for hero content | start of custom bootstrap grid system

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,23 @@
 import HeroContent from "./HeroContent";
+import HeroImage from "../assets/svgs/hero_image.svg";
 
 const Hero = () => (
-    <div className="hero container">
-                <HeroContent />
-        <div className="row">
-            <div className="col">
+    <section className="hero">
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-6">
+                    <HeroContent />
+                </div>
+                <div className="col-12 col-md-6">
+                    <img
+                        src={HeroImage}
+                        alt="Diamonds, stars, hearts, music notes in pastel, lilac and pink tones"
+                        className="hero__image"
+                    />
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 );
 
 export default Hero;
