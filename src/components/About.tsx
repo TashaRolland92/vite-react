0ea8723bdaae1020e7c6c9a34a67d445044b26f1
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-01-17 10:19:36 +0000

    increased selfie and emoji size

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,8 +6,8 @@ const About = () => {
 		<section id="section1" className="about">
 			<div className="container">
 				<div className="row">
-					<div className="col-12 align-centre">
-						<h2 className="about__title uppercase">About Me</h2>
+					<div className="col-12">
+						<h2 className="about__title align-centre uppercase">About Me</h2>
 					</div>
 					<div className="col-12 col-md-6">
 						<div className="about__image-container">
@@ -21,11 +21,11 @@ const About = () => {
 						</div>
 					</div>
 					<div className="col-12 col-md-6">
-						<p>👩‍💻 Highly skilled and adaptable Frontend Developer with expertise in both designing and developing web and platform applications, transforming design concepts and business needs into intuitive, user-friendly web experiences.</p>
-						<p>🎨 While I specialise in Frontend design and technologies, I am always eager to expand my skill set and learn the full development cycle. I've explored backend development, including working with databases, writing API methods and retieving and manipulating data via internal APIs. This hands on experience has given me a deeper appreciation for how frontend and backend systems interact and I love being part of the process that connects it all.</p>
-						<p>⚙️ I am passionate about writing clean, efficient and effective code and I enjoy keeping on top of industry trends and continuously enhancing my technical expertise with modern frameworks and tools.</p>
-						<p>🍸 I surprisingly do not like coffee - but I absolutely love an Espresso Martini! Nothing beats a Dirty Banana though, while sitting back relaxing on the beautiful white sanded beaches of Jamaica, you will catch me there from time to time.</p>
-						<p>✨ In my spare time, I enjoy crafting unique handmade items, which brings out my creative side, similar to web design. Just like designing for the web, crafting allows me to experiment, problem solve and turn ideas into tangible creations. It's a rewarding process when people love what you create and that fuels my passion for creativity and attention to detail.</p>
+						<p><span className="about__emoji">👩‍💻</span> Highly skilled and adaptable Frontend Developer with expertise in both designing and developing web and platform applications, transforming design concepts and business needs into intuitive, user-friendly web experiences.</p>
+						<p><span className="about__emoji">🎨</span> While I specialise in Frontend design and technologies, I am always eager to expand my skill set and learn the full development cycle. I've explored backend development, including working with databases, writing API methods and retieving and manipulating data via internal APIs. This hands on experience has given me a deeper appreciation for how frontend and backend systems interact and I love being part of the process that connects it all.</p>
+						<p><span className="about__emoji">⚙️</span> I am passionate about writing clean, efficient and effective code and I enjoy keeping on top of industry trends and continuously enhancing my technical expertise with modern frameworks and tools.</p>
+						<p><span className="about__emoji">🍸</span> I surprisingly do not like coffee - but I absolutely love an Espresso Martini! Nothing beats a Dirty Banana though, while sitting back relaxing on the beautiful white sanded beaches of Jamaica, you will catch me there from time to time.</p>
+						<p><span className="about__emoji">✨</span> In my spare time, I enjoy crafting unique handmade items, which brings out my creative side. Just like designing for the web, crafting allows me to experiment, problem solve and turn ideas into tangible creations. It's a rewarding process when people love what you create and that fuels my passion for creativity and attention to detail.</p>
 					</div>
 				</div>
 			</div>
