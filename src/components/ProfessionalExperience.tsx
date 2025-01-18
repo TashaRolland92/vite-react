3c2500aa0e1790a8c978f42ea824962c494bc8bb
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-01-18 21:28:34 +0000

    professional experience component | company logos | UI UX

--- src/components/ProfessionalExperience.tsx
+++ src/components/ProfessionalExperience.tsx
@@ -1,4 +1,10 @@
-import Timeline from "./Timeline";
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import BI from "../assets/professional/bi-logo.png";
+import EY from "../assets/professional/ey-logo.png";
+import NL from "../assets/professional/new-look-logo.png";
+import PP from "../assets/professional/pod-point-icon.png";
+import CD from "../assets/professional/copia-logo.png";
 
 const ProfessionalExperience = () => {
   return (
@@ -6,9 +12,75 @@ const ProfessionalExperience = () => {
 		<div className="container">
 			<div className="row">
 				<div className="col-12">
-					<h2>Professional Experience.</h2>
+					<h2 className="align-centre">Professional Experience.</h2>
 				</div>
-				<Timeline />
+
+				<VerticalTimeline>
+					<VerticalTimelineElement
+						className="vertical-timeline-element--work vertical-timeline-element--one"
+						contentStyle={{ background: '#FFD6FF', color: '#4E4A52', fontSize: '12px'}}
+						contentArrowStyle={{ borderRight: '14px solid #FFD6FF' }}
+						date="Sept 2020 - Nov 2024"
+						iconStyle={{ background: '#FFD6FF', color: '#FFF' }}
+						icon={<img src={BI} alt="Bordeaux Index" />}
+					>
+						<h3 className="vertical-timeline-element-title jordy-blue">Frontend Engineer</h3>
+						<h4 className="vertical-timeline-element-subtitle">Bordeaux Index</h4>
+						<p>JavaScript, PHP, Laravel, UI/UX, CSS3, MySql, Adobe Photoshop, Node.js, Figma, Jira, Azure, TDD, InVision.</p>
+					</VerticalTimelineElement>
+
+					<VerticalTimelineElement
+						className="vertical-timeline-element--work vertical-timeline-element--two"
+						date="Jan 2019 - Sept 2020"
+						contentStyle={{ background: '#E7C6FF', color: '#4E4A52', fontSize: '12px' }}
+						contentArrowStyle={{ borderRight: '14px solid #E7C6FF' }}
+						iconStyle={{ background: '#E7C6FF', color: '#4E4A52' }}
+						icon={<img src={EY} alt="Ernst & Young LLP" />}
+					>
+						<h3 className="vertical-timeline-element-title jordy-blue">Frontend Developer</h3>
+						<h4 className="vertical-timeline-element-subtitle">Ernst & Young LLP</h4>
+						<p>AngularJS, JavaScript, Trello, MS Sharepoint, HTML5, CSS3, CRM, InVision, Agile.</p>
+					</VerticalTimelineElement>
+
+					<VerticalTimelineElement
+						className="vertical-timeline-element--work vertical-timeline-element--three"
+						date="June 2019 - Dec 2019"
+						contentStyle={{ background: '#C8B6FF', color: '#4E4A52', fontSize: '12px' }}
+						contentArrowStyle={{ borderRight: '14px solid #C8B6FF' }}
+						iconStyle={{ background: '#C8B6FF', color: '#4E4A52' }}
+						icon={<img src={NL} alt="New Look" />}
+					>
+						<h3 className="vertical-timeline-element-title mimi-pink">Frontend Developer</h3>
+						<h4 className="vertical-timeline-element-subtitle">New Look</h4>
+						<p>JavaScript, CSS3, HTML5, Oracle Maxymiser, InVision, A/B Testing.</p>
+					</VerticalTimelineElement>
+
+					<VerticalTimelineElement
+						className="vertical-timeline-element--work vertical-timeline-element--four"
+						date="Nov 2018 - May 2019"
+						contentStyle={{ background: '#B8C0FF', color: '#4E4A52', fontSize: '12px' }}
+						contentArrowStyle={{ borderRight: '14px solid #B8C0FF' }}
+						iconStyle={{ background: '#B8C0FF', color: '#4E4A52' }}
+						icon={<img src={PP} alt="Pod Point" />}
+					>
+						<h3 className="vertical-timeline-element-title mimi-pink">Frontend Developer</h3>
+						<h4 className="vertical-timeline-element-subtitle">Pod Point</h4>
+						<p>JavaScript, CSS3, HTML5, React, NPM, React Native, Xcode, Node.js, AWS, Agile.</p>
+					</VerticalTimelineElement>
+
+					<VerticalTimelineElement
+						className="vertical-timeline-element--work vertical-timeline-element--five"
+						date="May 2012 - Oct 2015"
+						contentStyle={{ background: '#94B2F5', color: '#4E4A52', fontSize: '12px' }}
+						contentArrowStyle={{ borderRight: '14px solid #94B2F5' }}
+						iconStyle={{ background: '#94B2F5', color: '#4E4A52' }}
+						icon={<img src={CD} alt="Copia Digital" />}
+					>
+						<h3 className="vertical-timeline-element-title mimi-pink">Web Designer/Developer</h3>
+						<h4 className="vertical-timeline-element-subtitle">Copia Digital</h4>
+						<p>HTML5, CSS3, PHP, CMS, SVN, Adobe Photoshop, D3, Balsamiq, phpMyAdmin.</p>
+					</VerticalTimelineElement>
+				</VerticalTimeline>
 			</div>
 		</div>
     </section>
