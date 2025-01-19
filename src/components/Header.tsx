6a550ca0b7d7c82fc2767746cdc50efad5ef642d
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-01-19 19:56:11 +0000

    container class edited

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Nav from './Nav';
 const Header = () => {
 	return (
 		<header className="header">
-			<div className="container-lg">
+			<div className="container">
 				<div className="row">
 					<div className="col-12">
 						<Nav />
